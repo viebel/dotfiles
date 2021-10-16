4a23dbdcd0f3c3137aaf45ed75aef4027cf7684c
Yehonathan Sharvit <viebel@gmail.com>
2021-10-16 21:12:14 +0300

    nvim: Lua snip

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -42,6 +42,8 @@ require('packer').startup(function()
   use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp'
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
   use 'guns/vim-sexp'
   use 'SirVer/ultisnips'
   use 'honza/vim-snippets'
@@ -353,17 +355,62 @@ end
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
+-- luasnip setup
+local luasnip = require 'luasnip'
 
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
 -- Org
 -- init.lua
 require('orgmode').setup({
   org_agenda_files = {'~/Dropbox/TODOS/*'},
-    org_default_notes_file = '~/Dropbox/TODOS/refile.org',
+  org_default_notes_file = '~/Dropbox/TODOS/refile.org',
 })
 
 -- Firenvim
 if vim.g.started_by_firenvim then
-  vim.o.guifont = "Iosevka:h20"
+  vim.o.guifont = "SauceCodePro:h24"
   vim.o.laststatus = 0
 end
 
