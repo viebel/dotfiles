8caaf84f3e5623405b5a7798691430853e7d69e5
Yehonathan Sharvit <viebel@gmail.com>
2021-10-17 11:01:44 +0300

    nvim: keymap for clearing search highlight

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -115,6 +115,9 @@ vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
+-- Search
+vim.api.nvim_set_keymap('n', '<leader>sc', ':noh<CR>', { noremap = true, silent = true })
+
 --Remap for dealing with word wrap
 vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
 vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
