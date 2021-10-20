bcbe6f4953b131062e6b54fb8090859fb82b92f9
Yehonathan Sharvit <viebel@gmail.com>
2021-10-20 16:13:37 +0300

    nvim: folder tree doesn't focus on current buffer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -68,24 +68,13 @@ require('packer').startup(function()
       })
     end
   }
-use {
+  use {
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons',
     config = function() require'nvim-tree'.setup {
     view = {
         side = 'left'
-      },
-      -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-    update_focused_file = {
-    -- enables the feature
-        enable      = true,
-    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-    -- only relevant when `update_focused_file.enable` is true
-        update_cwd  = true,
-    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-        ignore_list = {}
-  }
+      }
     } end
   }
 end)
