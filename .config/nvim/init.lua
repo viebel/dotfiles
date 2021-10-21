df703b363ce8a4fa8b42eb61b1a5e48247f1992f
Yehonathan Sharvit <viebel@gmail.com>
2021-10-21 04:50:12 +0300

    nvim: Clojuredocs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,6 +18,9 @@ vim.api.nvim_exec(
 local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
+  use {'Olical/aniseed',  tag = "v3.23.0" }
+  use 'tami5/sqlite.lua'
+  use 'tami5/lispdocs.nvim'
   use 'tpope/vim-fugitive' -- Git commands in nvim
   use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
   use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
