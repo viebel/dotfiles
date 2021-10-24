160cbb14a764058bc7d46a61d39c682844169c06
Yehonathan Sharvit <viebel@gmail.com>
2021-10-24 23:00:05 +0300

    nvim: gitsigns blame and status

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -39,7 +39,25 @@ require('packer').startup(function()
   -- Add indentation guides even on blank lines
   use 'lukas-reineke/indent-blankline.nvim'
   -- Add git related info in the signs columns and popups
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use { 'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('gitsigns').setup {
+      signs = {
+        add = { hl = 'GitGutterAdd', text = '+' },
+        change = { hl = 'GitGutterChange', text = '~' },
+        delete = { hl = 'GitGutterDelete', text = '_' },
+        topdelete = { hl = 'GitGutterDelete', text = '‾' },
+        changedelete = { hl = 'GitGutterChange', text = '~' },
+      },
+      
+      current_line_blame = true,
+      current_line_blame_formatter_opts = {
+          relative_time = true
+      }
+    }
+    end
+  }
   -- Highlight, edit, and navigate code using a fast incremental parsing library
   use 'nvim-treesitter/nvim-treesitter'
   -- Additional textobjects for treesitter
@@ -144,11 +162,17 @@ vim.o.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'
 vim.o.fillchars = "fold: "
 vim.o.foldminlines = 1
 
+vim.g.gitsigns_status = function ()
+  return vim.b.gitsigns_status
+end
+
 --Set statusbar
 vim.g.lightline = {
   colorscheme = 'onedark',
-  active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' } } },
-  component_function = { gitbranch = 'fugitive#head' },
+  active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'gitstatus', 'readonly', 'filename', 'modified' } } },
+  component_function = {
+    gitstatus = 'gitsigns_status',
+    gitbranch = 'fugitive#head' },
 }
 
 local keyopts = { noremap = true, silent = true, exp = true } 
@@ -252,17 +276,6 @@ vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
 vim.g.indent_blankline_char_highlight = 'LineNr'
 vim.g.indent_blankline_show_trailing_blankline_indent = false
-
--- Gitsigns
-require('gitsigns').setup {
-  signs = {
-    add = { hl = 'GitGutterAdd', text = '+' },
-    change = { hl = 'GitGutterChange', text = '~' },
-    delete = { hl = 'GitGutterDelete', text = '_' },
-    topdelete = { hl = 'GitGutterDelete', text = '‾' },
-    changedelete = { hl = 'GitGutterChange', text = '~' },
-  },
-}
 
 -- Telescope
 local actions = require('telescope.actions')
