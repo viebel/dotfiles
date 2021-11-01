3d7310d526f6596a6a953959752f14501964831b
Yehonathan Sharvit <viebel@gmail.com>
2021-11-01 14:44:23 +0200

    nvim: Add Google translate plugin

--- .config/nvim/lua/my_plugins.lua
+++ .config/nvim/lua/my_plugins.lua
@@ -22,6 +22,7 @@ require('packer').startup(function()
       require('halonot').setup({main_key = 'w'})
     end
   }
+  use 'VincentCordobes/vim-translate'
   use 'wbthomason/packer.nvim' -- Package manager
   use {'Olical/aniseed',  tag = "v3.23.0" }
   use 'tami5/sqlite.lua'
