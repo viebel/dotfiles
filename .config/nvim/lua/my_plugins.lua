e674e0bd8d981456a8a4eb93f7f5dd5328e75b78
Yehonathan Sharvit <viebel@gmail.com>
2021-11-05 07:31:55 +0200

    nvim: remove unused plugins

--- .config/nvim/lua/my_plugins.lua
+++ .config/nvim/lua/my_plugins.lua
@@ -37,8 +37,6 @@ require('packer').startup(function()
   use 'tpope/vim-abolish'
   use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
   use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
-  -- use 'ludovicchabant/vim-gutentags' -- Automatic tags management
-  -- UI to select things (files, grep results, open buffers...)
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use { 'nvim-telescope/telescope-github.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
@@ -47,7 +45,6 @@ require('packer').startup(function()
   use 'itchyny/lightline.vim' -- Fancier statusline
   -- Add indentation guides even on blank lines
   use 'lukas-reineke/indent-blankline.nvim'
-  -- Add git related info in the signs columns and popups
   use { 'lewis6991/gitsigns.nvim',
     requires = { 'nvim-lua/plenary.nvim' },
     config = function()
@@ -71,8 +68,6 @@ require('packer').startup(function()
   use 'nvim-treesitter/nvim-treesitter'
   -- Additional textobjects for treesitter
   use 'nvim-treesitter/nvim-treesitter-textobjects'
-  use 'Xuyuanp/nerdtree-git-plugin'
-  use 'ryanoasis/vim-devicons'
   use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
   use 'onsails/lspkind-nvim'
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
@@ -96,8 +91,6 @@ require('packer').startup(function()
   use { 'neoclide/coc.nvim', branch= 'release' }
   use {'junegunn/fzf',  run = function() vim.fn['fzf#install']() end}
   use 'junegunn/fzf.vim'
-  use 'antoinemadec/coc-fzf'
-  -- use 'kristijanhusak/orgmode.nvim'
   use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
   use { "AckslD/nvim-neoclip.lua",
     requires = {'tami5/sqlite.lua', module = 'sqlite'},
