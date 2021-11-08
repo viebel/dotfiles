d4cc2f23a585d325c0ae83a2d653f7caab048700
Yehonathan Sharvit <viebel@gmail.com>
2021-11-08 21:33:17 +0200

    nvim: Add custom telecope functions

--- .config/nvim/lua/my_telescope.lua
+++ .config/nvim/lua/my_telescope.lua
@@ -3,9 +3,35 @@ local M = {}
 function M.edit_neovim()
   require('telescope.builtin').find_files {
       cwd = "~/.config/nvim/",
-      hidden = false,
+      hidden = true,
       prompt_title = "nvim dot files"
 }
 end
+
+function M.grep_neovim_files()
+  require('telescope.builtin').live_grep {
+      cwd = "~/.config/nvim/",
+      prompt_title = "grep nvim files"
+}
+end
+
+function M.find_configs()
+  require("telescope.builtin").find_files {
+    prompt_title = " NVim & Term Config Find",
+    results_title = "Config Files Results",
+    path_display = { "smart" },
+    hidden = true,
+    cwd = "~/dotfiles/",
+  }
+end
+
+function M.grep_configs()
+  require("telescope.builtin").live_grep {
+    prompt_title = " Configs Grep",
+    results_title = "Config Results",
+    cwd = "~/dotfiles",
+  }
+end
+
 
 return M
