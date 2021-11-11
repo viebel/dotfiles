9ea43810dee3e854da9a9323788a02e53afd647e
Yehonathan Sharvit <viebel@gmail.com>
2021-11-11 19:43:06 +0200

    nvim: Add keymap to grep nvim files via telescope

--- .config/nvim/lua/my_stuff.lua
+++ .config/nvim/lua/my_stuff.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_keymap('n', '<leader>en', ':lua require("my_telescope").edit_neovim()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gn', ':lua require("my_telescope").grep_neovim_files()<CR>', { noremap = true, silent = true })
  
