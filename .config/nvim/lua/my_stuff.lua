729f6858ce79089c343951999be5285cc4955d62
Yehonathan Sharvit <viebel@gmail.com>
2021-11-14 00:27:17 +0200

    nvim: Additional keymaps for config files

--- .config/nvim/lua/my_stuff.lua
+++ .config/nvim/lua/my_stuff.lua
@@ -1,3 +1,5 @@
 vim.api.nvim_set_keymap('n', '<leader>en', ':lua require("my_telescope").edit_neovim()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gn', ':lua require("my_telescope").grep_neovim_files()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ec', ':lua require("my_telescope").find_configs()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>c/', ':lua require("my_telescope").grep_configs()<CR>', { noremap = true, silent = true })
  
