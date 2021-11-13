dd7b965eb3b42e43cfa1dffd1cbcb3d0e8afc9ad
Yehonathan Sharvit <viebel@gmail.com>
2021-11-14 00:27:44 +0200

    nvim: Clipboard Integration for WSL

---
+++ .config/nvim/after/plugin/wsl.lua
@@ -0,0 +1,20 @@
+local is_wsl = (function()
+  local output = vim.fn.systemlist "uname -r"
+  return not not string.find(output[1] or "", "WSL")
+end)()
+
+if is_wsl then
+  vim.g.clipboard = {
+    name = 'win32yank-wsl',
+    copy = {
+      ['+'] = 'win32yank.exe -i --crlf',
+      ['*'] = 'win32yank.exe -i --crlf',
+
+    },
+    paste = {
+      ['+'] = 'win32yank.exe -o --lf',
+      ['*'] = 'win32yank.exe -o --lf',
+    },
+    cache_enabled = 0,
+  }
+end
